5585c93ddc2c0858b3bec0e6d05498393ef6e27e
shravyaburla <shravya083@gmail.com>
2025-03-15 11:00:10 +0530

    bucket change

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "emrcicdpr22025" # Replace with your actual S3 bucket name
+    bucket = "awsterraeksdeploy" # Replace with your actual S3 bucket name
     key    = "Eks/terraform.tfstate"
     region = "ap-south-1"
   }
